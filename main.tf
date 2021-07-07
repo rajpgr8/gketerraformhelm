9490ce7bb7695f27654fe0952f3a6177cb7cfda9
rajpgr8 <rajp.gr8@gmail.com>
2021-07-08 00:05:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,43 +4,6 @@ provider "google" {
   region      = var.region
 }
 
-data "google_client_config" "default" {
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = google_container_cluster.primary.endpoint
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-  }
-}
-
-resource "helm_release" "nginx" {
-  namespace = "default"
-  name  = "nginx-release"
-
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
-  version    = "9.3.6"
-  
-  set {
-    name  = "autoscaling.enabled"
-    value = "true"
-  }
-  set {
-    name  = "autoscaling.minReplicas"
-    value = 2
-  }
-  set {
-    name  = "autoscaling.maxReplicas"
-    value = 6
-  }
-  set {
-    name  = "autoscaling.targetCPU"
-    value = 50
-  }
-}
-
 resource "google_container_cluster" "primary" {
   name                     = var.cluster_name
   location                 = var.region
